12818dfb0577dca84f4f0b25ab9abcd22f5ef90a
mathiasbruun <math686f@edu.easj.dk>
2018-08-31 13:02:55 +0200

    Ny med AES kryptering!

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 
         System.out.println("|--------------------------------|");
@@ -12,7 +12,9 @@ public class Main {
         System.out.println("|            Krypteret.          |");
         System.out.println("|                                |");
         System.out.println("|     Tast 1 for Kryptering      |");
-        System.out.println("|     Tast 2 for dekryptering    |");
+        System.out.println("|    Tast 2 for dekryptering     |");
+        System.out.println("|   Tast 3 for AES kryptering    |");
+        System.out.println("|   Tast 4 for AES dekryptering  |");
         System.out.println("|--------------------------------|");
         Scanner scanner = new Scanner(System.in);
         System.out.print("$");
@@ -21,6 +23,7 @@ public class Main {
 
         Stringkrypt stringkrypt = new Stringkrypt();
         Stringkrypt beskeddekrypt = new Stringkrypt();
+        AESenc aeSenc = new AESenc();
 
 if (besked.contains("1")){
     System.out.print("Indsæt din besked her: ");
@@ -31,6 +34,17 @@ if (besked.contains("2")){
     System.out.print("Indsæt din besked her: ");
     String beskedkrypt = scanner.nextLine();
     beskeddekrypt.dekrypterny(beskedkrypt);
+        }
+
+if (besked.contains("3")){
+    System.out.println("Indsæt din besked her: ");
+    String beskedAES = scanner.nextLine();
+    System.out.println(aeSenc.encrypt(beskedAES));
+}
+if (besked.contains("4")){
+    System.out.println("Indsæt din besked her: ");
+    String beskedAES = scanner.nextLine();
+    System.out.println(aeSenc.decrypt(beskedAES));
         }
     }
 
