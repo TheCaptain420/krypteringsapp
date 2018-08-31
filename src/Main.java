123eacbd693d0bfad5464ededdd943c712863682
mathiasbruun <math686f@edu.easj.dk>
2018-08-31 11:52:25 +0200

    Console UI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        System.out.println("|--------------------------------|");
+        System.out.println("|                                |");
+        System.out.println("|      Velkommen til krypter     |");
+        System.out.println("|  Indtast din besked du ønsker  |");
+        System.out.println("|            Krypteret.          |");
+        System.out.println("|                                |");
+        System.out.println("|--------------------------------|");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("$");
+        String besked = scanner.nextLine();
+
+        System.out.println(besked);
+
+
+
     }
 }
